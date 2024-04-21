774627c553bb597d2576aa7e1e60daae8620150c
unknown <davidkeukeu8@gmail.com>
2024-04-22 01:39:59 +0300

    cotisation always

--- start/routes/cotisation.ts
+++ start/routes/cotisation.ts
@@ -1,7 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.post('cotisation/:activityId', 'CotisationsController.store')
+  Route.post('cotisation/', 'CotisationsController.store')
   Route.get('cotisation', 'CotisationsController.index')
   Route.get('cotisation/:activityId', 'CotisationsController.getAllByActivity')
   Route.get('cotisationByMember/:memberId', 'CotisationsController.getAllByMember')
